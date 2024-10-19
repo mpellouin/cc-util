7eb656f4f7e73d464a7e919df616ae8f7662e46c
mpellouin <maxence.pellouin@epitech.eu>
2024-10-19 10:04:00 +0200

    init: add script

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const logoLink = document.querySelector('.logo__link');
+
+    if (logoLink) {
+        logoLink.setAttribute('href', 'https://coudrecoudre.com');
+    }
+});
